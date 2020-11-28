51ededc093213c7469f76ec0e33d3960a274f9d5
Abbey Tipton <39957439+abbeytipton@users.noreply.github.com>
2020-11-28 14:49:54 -0600

    updating formatting/comments/organization

--- Portfolio/JS/Script.js
+++ Portfolio/JS/Script.js
@@ -1,33 +1,154 @@
-﻿$(document).ready(function () {
+﻿// On page load //
+$(document).ready(function () {
 
-
+    // After two seconds, load the intro header //
     setTimeout(
         function () {
-            $("#introHeader").fadeIn("slow");
+            $("#IntroHeader").fadeIn("slow");
         }, 2000);
 
+    // After four seconds, load the intro text //
     setTimeout(
         function () {
-            $("#introText").fadeIn("slow");
+            $("#IntroText").fadeIn("slow");
         }, 4000);
 
+    // After six seconds, load the projects arrow //
     setTimeout(
         function () {
-            $(".downArrow").fadeIn("slow");
+            $(".DownArrow").fadeIn("slow");
         }, 6000);
+
+    // Set color of the left picture button to grey, so it can't be clicked //
+    $(".PictureButtonLeft").css('color', 'grey');
+    // Hide the icon background //
+    $(".IconBackground").hide();
 });
 
-$(".downArrow").click(function () {
-    $(".jumbotron").slideUp(function () {
-        $("#PageOne").slideDown(function() {
-            $(".seeNextArrow").fadeIn(function () {
-                BounceUp("seeNextArrow", 3);
+
+// Project Modals //
+
+// Set up project pictures and text for the modals to use as the user looks at more pictures //
+var Project1Pictures = ['~/Images/Projects/Project1-1.png', '~/Images/Projects/Project1-2.png', '~/Images/Projects/Project1-3.png', '~/Images/Projects/Project1-4.png', '~/Images/Projects/Project1-5.png', '~/Images/Projects/Project1-6.png', '~/Images/Projects/Project1-7.png', '~/Images/Projects/Project1-8.png'];
+var Project1Text = ['Scrum Flow\'s main functionality is as a digital Scrum Board. Here you can see the Scrum Board layout; each card represents a task. Users can drag their tasks to each category as they work on them.', 'blah2', 'blah3', 'blah4', 'blah5', 'blah6', 'blah7', 'blah8'];
+
+// Set up the current project information to be project one; this will change when the user clicks on a different project //
+var CurrentProjectPictures = Project1Pictures;
+var CurrentProjectText = Project1Text;
+var CurrentPictureSource = "Project1Image";
+var CurrentTextSource = "Project1Text";
+var CurrentIndex = 0;
+var ContinueLeftArrow = false;
+var ContinueRightArrow = true;
+
+// This method checks the arrows in the project view modals to see if they need to be shown or not //
+function CheckProjectArrows() {
+    // If there are no more pictures to the right, then make the right arrow gray and add the no Hover class //
+    if (CurrentIndex == CurrentProjectPictures.length - 1) {
+        $(".PictureButtonRight").css('color', 'grey');
+        $(".PictureButtonRight").addClass('NoHover');
+        $(".PictureButtonRight").removeClass('Hover');
+        ContinueRightArrow = false;
+    }
+    // Otherwise, make the arrow the right color and add the Hover class //
+    else {
+        $(".PictureButtonRight").css('color', '#bc8f8f');
+        $(".PictureButtonRight").addClass('Hover');
+        $(".PictureButtonRight").removeClass('NoHover');
+        ContinueRightArrow = true;
+    }
+
+    // If there are no more pictures to the left, then make the left arrow gray and add the no Hover class //
+    if (CurrentIndex == 0) {
+        $(".PictureButtonLeft").css('color', 'grey');
+        $(".PictureButtonLeft").addClass('NoHover');
+        $(".PictureButtonLeft").removeClass('Hover');
+        ContinueLeftArrow = false;
+    }
+    // Otherwise, make the arrow the right color and add the Hover class //
+    else {
+        $(".PictureButtonLeft").css('color', '#bc8f8f');
+        $(".PictureButtonLeft").addClass('Hover');
+        $(".PictureButtonLeft").removeClass('NoHover');
+        ContinueLeftArrow = true;
+    }
+}
+
+// Click function on the right picture button //
+$(".PictureButtonRight").click(function () {
+    // If we're going to continue //
+    if (ContinueRightArrow) {
+        CurrentIndex++;
+        CheckProjectArrows();
+        // Update the picture with the next one in the array and then update the text to match //
+        $('#' + CurrentPictureSource).attr('src', Project1Pictures[CurrentIndex]);
+        $('#' + CurrentTextSource).text(CurrentProjectText[CurrentIndex]);
+    }
+});
+
+// Click function on the left picture button //
+$(".PictureButtonLeft").click(function () {
+    // If we're going to continue //
+    if (ContinueLeftArrow) {
+        CurrentIndex--;
+        CheckProjectArrows();
+        // Update the picture with the next one in the array and then update the text to match //
+        $('#' + CurrentPictureSource).attr('src', Project1Pictures[CurrentIndex]);
+        $('#' + CurrentTextSource).text(CurrentProjectText[CurrentIndex]);
+    }
+});
+
+// On click of the zoom image, we need to make the current picture larger, so show the large picture modal with the picture in it //
+$(".ZoomImage").click(function () {
+    $('.LargePicture').show("scale", { percent: 100, direction: 'vertical' }, 1000);
+    $(".IconBackground").show();
+});
+
+// Project button one click function - show the project 1 modal //
+$("#Project1Btn").click(function () {
+    $("#Project1Modal").show();
+    return false;
+});
+
+// Close button click function for the project modals - closes the modal // 
+$(".Close").click(function () {
+    $(".Modal").fadeOut();
+});
+
+
+// Main Page //
+
+var CurrentPage = "PageOne";
+var CurrentProject = "Jumbotron";
+var AnimationFinished = true;
+var BounceUpDone = false;
+var BounceDownDone = false;
+
+// Click function from the arrows on the original page load //
+$(".DownArrow").click(function () {
+    // Slide up the jumbotron and down the first page //
+    $(".Jumbotron").slideUp(function () {
+        $("#PageOne").slideDown(function () {
+            // Fade in the see next arrow and bounce it 3 times //
+            $(".SeeNextArrow").fadeIn(function () {
+                BounceUp("SeeNextArrow", 3);
             });
         });
     });
 });
 
-$("#linkedInBtn").click(function () {
+// Click function for the about button - show the about page //
+$("#AboutBtn").click(function () {
+    ShowPage("AboutPage");
+});
+
+// Click function for the contact button - show the contact page //
+$("#ContactBtn").click(function () {
+    ShowPage("ContactPage");
+});
+
+// Click function for the linkedin  button - open the page and try for a new tab //
+$("#LinkedInBtn").click(function () {
     var win = window.open('//www.linkedin.com/in/abbey-tipton/', '_blank');
     if (win) {
         win.focus();
@@ -36,7 +157,8 @@ $("#linkedInBtn").click(function () {
     }
 });
 
-$("#githubBtn").click(function () {
+// Click function for the github button - open the page and try for a new tab //
+$("#GithubBtn").click(function () {
     var win = window.open('https://github.com/abbeytipton', '_blank');
     if (win) {
         win.focus();
@@ -45,7 +167,8 @@ $("#githubBtn").click(function () {
     }
 });
 
-$("#codepenBtn").click(function () {
+// Click function for the codepen button - open the page and try for a new tab //
+$("#CodepenBtn").click(function () {
     var win = window.open('https://codepen.io/abbeytipton', '_blank');
     if (win) {
         win.focus();
@@ -54,51 +177,153 @@ $("#codepenBtn").click(function () {
     }
 });
 
-$(".seeNextArrow").click(function () {
-    var currentPage = $('.projectsView:visible').prop('id');
-    var nextPage = $("#" + currentPage).next('.projectsView').attr("id");
-    $(".seeNextArrow, .seePreviousArrow").hide();
+// Function to show the given page //
+function ShowPage(pageToShow) {
+    // Hide the arrows on the side of the screen //
+    HideArrows();
+    // Get the currnet page and then slide it up //
+    GetCurrentPage();
+    $("#" + CurrentPage).slideUp(function () {
+        // Slide down the page to show //
+        $("#" + pageToShow).slideDown();
+    });
+}
+
+function GetCurrentPage() {
+    // Get the visible page - it's the projects view that isn't hidden //
+    var visiblePage = $("div.ProjectsView").not(":hidden").prop("id");
+    // If the visible page isn't null, then set that page to variables //
+    if (visiblePage != null) {
+        CurrentPage = visiblePage;
+        CurrentProject = visiblePage;
+    }
+    // Otherwise, an "other" page is showing, so set that page as the current page //
+    else {
+        visiblePage = $("div.OtherPageView").not(":hidden").prop("id");
+        if (visiblePage != null) {
+            CurrentPage = visiblePage;
+        }
+        // If no pages are visible, then the current page is the jumbotron //
+        else {
+            CurrentPage = "Jumbotron";
+        }
+    }
+}
+
+// Hide the see next/previous arrows //
+function HideArrows() {
+    $(".SeeNextArrow").fadeOut();
+    $(".SeePreviousArrow").fadeOut();
+}
+
+// See next page arrow button click //
+$(".SeeNextArrow").click(function () {
+    // Get the current page (the visible projects page) and the next page //
+    var currentPage = $('.ProjectsView:visible').prop('id');
+    var nextPage = $("#" + currentPage).next('.ProjectsView').attr("id");
+    // Hide the arrows and slide up the current page and down the next page //
+    HideArrows();
     $("#" + currentPage).slideUp("slow", function () {
         $("#" + nextPage).slideDown("slow");
     });
-
+    // Handle the arrows (might need to hide or show depending on what page was shown //
     HandlePageArrows(currentPage);
 });
 
-$(".seePreviousArrow").click(function () {
-    var currentPage = $('.projectsView:visible').prop('id');
-    var previousPage = $("#" + currentPage).prev('.projectsView').attr("id");
-    $(".seePreviousArrow, .seePreviousArrow").hide();
+// See previous page arrow button click //
+$(".SeePreviousArrow").click(function () {
+    // Get the current page (the visible projects page) and the previous page //
+    var currentPage = $('.ProjectsView:visible').prop('id');
+    var previousPage = $("#" + currentPage).prev('.ProjectsView').attr("id");
+    // Hide the arrows and then slide up the current page and down the previous page //
+    HideArrows();
     $("#" + currentPage).slideUp("slow", function () {
         $("#" + previousPage).slideDown("slow");
     });
-
+    // Handle the arrows (might need to hide or show depending on what page was shown //
     HandlePageArrows(currentPage);
 });
 
 function HandlePageArrows(currentPage) {
-    var remainingPages = $("#" + currentPage).next('.projectsView').length;
+    // Get the remaining pages based off the passed in current page //
+    var remainingPages = $("#" + currentPage).next('.ProjectsView').length;
+    // If there are remaining pages, then we need to show the next button //
     if (remainingPages != 1) {
-        $(".seeNextArrow").fadeIn();
-        BounceUp("seeNextArrow", 3);
+        $(".SeeNextArrow").fadeIn();
+        // Only bounce the arrow if it hasn't been bounced already //
+        if (BounceUpDone == false) {
+            BounceUp("SeeNextArrow", 3);
+        }
     }
-    var previousPages = $("#" + currentPage).prev('.projectsView').length;
+    // If there are previous pages, then we need to show the previous button //
+    var previousPages = $("#" + currentPage).prev('.ProjectsView').length;
     if (previousPages != 1) {
-        $(".seePreviousArrow").fadeIn();
-        BounceDown("seePreviousArrow", 3);
+        $(".SeePreviousArrow").fadeIn();
+        // Only bounce the arrow if it hasn't been bounced already //
+        if (BounceDownDone == false) {
+            BounceDown("SeePreviousArrow", 3);
+        }
     }
 };
 
 function BounceUp(element, times) {
+    AnimationFinished = false;
+    // For the number of times passed in, move the element up //
     for (i = 0; i < times; i++) {
         $("." + element).show().animate({ marginBottom: "80px" }, 400).animate({ marginBottom: "40px" }, 400);
     }
-    $("." + element).show().animate({ marginBottom: "0px" }, 400).animate({ marginBottom: "0px" }, 400);
+    // After bouncing, move the element back to where it started and set the bool to true //
+    $("." + element).show().animate({ marginBottom: "0px" }, 400).animate({ marginBottom: "0px" }, 400, function () {
+        AnimationFinished = true;
+    });
+    // Set this bool to true so we don't bounce the arrows every time the user clicks them, only the first time //
+    BounceUpDone = true;
 }
 
 function BounceDown(element, times) {
+    AnimationFinished = false;
+    // For the number of times passed in, move the element down //
     for (i = 0; i < times; i++) {
         $("." + element).show().animate({ marginTop: "80px" }, 400).animate({ marginTop: "40px" }, 400);
     }
-    $("." + element).show().animate({ marginTop: "0px" }, 400).animate({ marginTop: "0px" }, 400);
+    // After bouncing, move the element back to where it started and set the bool to true //
+    $("." + element).show().animate({ marginTop: "0px" }, 400).animate({ marginTop: "0px" }, 400, function () {
+        AnimationFinished = true;
+    });
+    // Set this bool to true so we don't bounce the arrows every time the user clicks them, only the first time //
+    BounceDownDone = true;
 }
+
+// Close button click function (this will be for "other" pages always //
+$(".CloseBtn").click(function () {
+    // Get the visible page and slide it up //
+    var visiblePage = $("div.OtherPageView").not(":hidden").prop("id");
+    $("#" + visiblePage).slideUp(function () {
+        // Slide down the current project page //
+        $("#" + CurrentProject).slideDown();
+        // If the current project is not the jumbotron, then we need to set up the arrows for the page // 
+        if (CurrentProject != "Jumbotron") {
+            var nextPage = $("#" + CurrentProject).next('.ProjectsView').prop("id");
+            if (nextPage == null) {
+                nextPage = $("#" + CurrentProject).prev('.ProjectsView').prop("id");
+            }
+            HandlePageArrows(nextPage);
+        }
+    });
+});
+
+
+// Captcha //
+
+// Checks for the Captcha and if it hasn't been checked, then shows the red text underneat it //
+function validateRecaptcha() {
+    var response = grecaptcha.getResponse();
+    if (response.length === 0) {
+        $("#ErrorLabelForCaptcha").text("Please check the box above to indicate that you are not a robot.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
