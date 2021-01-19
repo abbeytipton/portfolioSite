a0a6801cb86605167f6a1c966a3a4b69db140020
Abbey Tipton <39957439+abbeytipton@users.noreply.github.com>
2021-01-18 20:32:33 -0600

    Delete Portfolio directory

--- Portfolio/CaptchaSuccess.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Portfolio
-{
-   public class CaptchaSuccess
-        {
-            // Returns success if the Google response comes back correctly //
-            public string success { get; set; }
-        }
-    }
