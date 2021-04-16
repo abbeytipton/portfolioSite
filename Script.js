daa8500818ac8220c28c6d0c267e17e6143514eb
Abbey Tipton <39957439+abbeytipton@users.noreply.github.com>
2021-04-16 17:44:44 -0500

    Finished all modals, added slick for main pages

--- Script.js
+++ Script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
           autoSize : false,
           scrolling : 'auto',
           fitToView : false,
-          width : '10000px',
+          width : '5000px',
           autoWidth: false,
           autoHeight: false,
           autoDimensions: false,
@@ -44,8 +44,21 @@ $(document).ready(function () {
           nextArrow: '<button class="SlickButtonRight"><i class="fas fa-arrow-circle-right"></i></button>',
         });
 
-        // Set this div to be flex - the slick plugin strips it of this style //
+        // Set up the slick plugin for the project pages //
+        $('.ProjectSlick').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          vertical: true,
+          speed: 400,
+          autoplay: false,
+          nextArrow: '<button class="SlickButtonDown"><i class="fas fa-arrow-circle-down"></i></button>',
+          prevArrow: '<button class="SlickButtonUp"><i class="fas fa-arrow-circle-up"></i></button>',
+        });
+
+        // Set these divs to be flex - the slick plugin strips them of this style //
         $(".ModalFlex").css('display', 'flex');
+        $(".ProjectsFlex").css('display', 'flex');
       });
 
       // Modals //
@@ -55,41 +68,101 @@ $(document).ready(function () {
         $("#ProjectOneModal").css('height', '100%');
       });
 
+      // Project button two click function - show the project modal for project two //
+      $("#Project2Btn").click(function () {
+        $("#ProjectTwoModal").css('height', '100%');
+      });
+
+      // Project button three click function - show the project modal for project three //
+      $("#Project3Btn").click(function () {
+        $("#ProjectThreeModal").css('height', '100%');
+      });
+
+      // Project button four click function - show the project modal for project four //
+      $("#Project4Btn").click(function () {
+        $("#ProjectFourModal").css('height', '100%');
+      });
+
       // Close button click function for the project modals - closes the modal //
       $(".Close").click(function () {
-        $(".Modal").fadeOut();
+        $(".Modal").css('height', '0%');
       });
 
       // Main Page //
-
-      var CurrentPage = "PageOne";
-      var CurrentProject = "Jumbotron";
-      var AnimationFinished = true;
-      var BounceUpDone = false;
-      var BounceDownDone = false;
 
       // Click function from the arrows on the original page load //
       $(".DownArrow").click(function () {
         // Slide up the jumbotron and down the first page //
         $(".Jumbotron").slideUp(function () {
-          $("#PageOne").slideDown(function () {
-            // Fade in the see next arrow and bounce it 3 times //
-            $(".SeeNextArrow").fadeIn(function () {
-              BounceUp("SeeNextArrow", 3);
-            });
-          });
+          $('.ProjectPages').animate({height:'100vh'},500);
         });
       });
 
+      // Show page function for "other" pages (about and contact) //
+      function ShowPage(pageToShow) {
+        // Make the close button invisible //
+        $('.CloseBtn').css('color', 'transparent');
+
+        // If the jumbotron is not showing, then the project pages are. So slide them up, then slide down the page to show //
+        if (!$(".Jumbotron").is(":visible")) {
+          $('.ProjectPages').animate({height:'0vh'},500, function() {
+            $("#" + pageToShow).slideDown(500, function() {
+              // Show the close button now that everthing is loaded - it hops and looks strange otherwise //
+              $(".CloseBtn").css('color', '#A5BEA8');
+            });
+          });
+        }
+        // If the jumbotron is showing, just slide down the page to show and show the close button //
+        else {
+          $("#" + pageToShow).slideDown(500, function() {
+            $(".CloseBtn").css('color', '#A5BEA8');
+          });
+        }
+      }
+
       // Click function for the about button - show the about page //
       $("#AboutBtn").click(function () {
-        ShowPage("AboutPage");
+        // If the contact page is visible, then hide it first //
+        if ($("#ContactPage").is(":visible")) {
+          $("#ContactPage").slideUp(500, function() {
+            ShowPage("AboutPage");
+          });
+        }
+        // Otherwise, just show the about page //
+        else {
+          ShowPage("AboutPage");
+        }
       });
 
       // Click function for the contact button - show the contact page //
       $("#ContactBtn").click(function () {
-        ShowPage("ContactPage");
+        // If the about page is visible, then hide it first //
+        if ($("#AboutPage").is(":visible")) {
+          $("#AboutPage").slideUp(500, function() {
+            ShowPage("ContactPage");
+          });
+        }
+        // Otherwise, just show the contact page //
+        else {
+          ShowPage("ContactPage");
+        }
       });
+
+      // Close button click function (this will be for "other" pages) //
+      $(".CloseBtn").click(function () {
+        // Make this item transparent (the close button)
+        $(this).css('color', 'transparent');
+        // Get the visible page and slide it up //
+        var visiblePage =  $(this).closest('.OtherPageView').attr('id');
+        $("#" + visiblePage).slideUp(500, function() {
+          // If the jumbotron isn't showing, then we need to slide down the project pages //
+          if (!$(".Jumbotron").is(":visible")) {
+            $('.ProjectPages').animate({height:'100vh'},500);
+          }
+        });
+      });
+
+      // Buttons //
 
       // Click function for the linkedin  button - open the page and try for a new tab //
       $("#LinkedInBtn").click(function () {
@@ -119,141 +192,6 @@ $(document).ready(function () {
         } else {
           win = window.open('https://codepen.io/abbeytipton');
         }
-      });
-
-      // Function to show the given page //
-      function ShowPage(pageToShow) {
-        // Hide the arrows on the side of the screen //
-        HideArrows();
-        // Get the currnet page and then slide it up //
-        GetCurrentPage();
-        $("#" + CurrentPage).fadeOut(function () {
-          // Slide down the page to show //
-          $("#" + pageToShow).fadeIn();
-        });
-      }
-
-      function GetCurrentPage() {
-        // Get the visible page - it's the projects view that isn't hidden //
-        var visiblePage = $("div.ProjectsView").not(":hidden").prop("id");
-        // If the visible page isn't null, then set that page to variables //
-        if (visiblePage != null) {
-          CurrentPage = visiblePage;
-          CurrentProject = visiblePage;
-        }
-        // Otherwise, an "other" page is showing, so set that page as the current page //
-        else {
-          visiblePage = $("div.OtherPageView").not(":hidden").prop("id");
-          if (visiblePage != null) {
-            CurrentPage = visiblePage;
-          }
-          // If no pages are visible, then the current page is the jumbotron //
-          else {
-            CurrentPage = "Jumbotron";
-          }
-        }
-      }
-
-      // Hide the see next/previous arrows //
-      function HideArrows() {
-        $(".SeeNextArrow").fadeOut();
-        $(".SeePreviousArrow").fadeOut();
-      }
-
-      // See next page arrow button click //
-      $(".SeeNextArrow").click(function () {
-        // Get the current page (the visible projects page) and the next page //
-        var currentPage = $('.ProjectsView:visible').prop('id');
-        var nextPage = $("#" + currentPage).next('.ProjectsView').attr("id");
-        // Hide the arrows and slide up the current page and down the next page //
-        HideArrows();
-        $("#" + currentPage).slideUp("slow", function () {
-          $("#" + nextPage).slideDown("slow");
-        });
-        // Handle the arrows (might need to hide or show depending on what page was shown //
-        HandlePageArrows(currentPage);
-      });
-
-      // See previous page arrow button click //
-      $(".SeePreviousArrow").click(function () {
-        // Get the current page (the visible projects page) and the previous page //
-        var currentPage = $('.ProjectsView:visible').prop('id');
-        var previousPage = $("#" + currentPage).prev('.ProjectsView').attr("id");
-        // Hide the arrows and then slide up the current page and down the previous page //
-        HideArrows();
-        $("#" + currentPage).slideUp("slow", function () {
-          $("#" + previousPage).slideDown("slow");
-        });
-        // Handle the arrows (might need to hide or show depending on what page was shown //
-        HandlePageArrows(currentPage);
-      });
-
-      function HandlePageArrows(currentPage) {
-        // Get the remaining pages based off the passed in current page //
-        var remainingPages = $("#" + currentPage).next('.ProjectsView').length;
-        // If there are remaining pages, then we need to show the next button //
-        if (remainingPages != 1) {
-          $(".SeeNextArrow").fadeIn();
-          // Only bounce the arrow if it hasn't been bounced already //
-          if (BounceUpDone == false) {
-            BounceUp("SeeNextArrow", 3);
-          }
-        }
-        // If there are previous pages, then we need to show the previous button //
-        var previousPages = $("#" + currentPage).prev('.ProjectsView').length;
-        if (previousPages != 1) {
-          $(".SeePreviousArrow").fadeIn();
-          // Only bounce the arrow if it hasn't been bounced already //
-          if (BounceDownDone == false) {
-            BounceDown("SeePreviousArrow", 3);
-          }
-        }
-      };
-
-      function BounceUp(element, times) {
-        AnimationFinished = false;
-        // For the number of times passed in, move the element up //
-        for (i = 0; i < times; i++) {
-          $("." + element).show().animate({ marginBottom: "80px" }, 400).animate({ marginBottom: "40px" }, 400);
-        }
-        // After bouncing, move the element back to where it started and set the bool to true //
-        $("." + element).show().animate({ marginBottom: "0px" }, 400).animate({ marginBottom: "0px" }, 400, function () {
-          AnimationFinished = true;
-        });
-        // Set this bool to true so we don't bounce the arrows every time the user clicks them, only the first time //
-        BounceUpDone = true;
-      }
-
-      function BounceDown(element, times) {
-        AnimationFinished = false;
-        // For the number of times passed in, move the element down //
-        for (i = 0; i < times; i++) {
-          $("." + element).show().animate({ marginTop: "80px" }, 400).animate({ marginTop: "40px" }, 400);
-        }
-        // After bouncing, move the element back to where it started and set the bool to true //
-        $("." + element).show().animate({ marginTop: "0px" }, 400).animate({ marginTop: "0px" }, 400, function () {
-          AnimationFinished = true;
-        });
-        // Set this bool to true so we don't bounce the arrows every time the user clicks them, only the first time //
-        BounceDownDone = true;
-      }
-
-      // Close button click function (this will be for "other" pages always //
-      $(".CloseBtn").click(function () {
-        // Get the visible page and slide it up //
-        var visiblePage = $("div.OtherPageView").not(":hidden").prop("id");
-        $("#" + visiblePage).fadeOut(function () {
-          // Slide down the current project page //
-          $("#" + CurrentProject).fadeIn();
-          // If the current project is not the jumbotron, then we need to set up the arrows for the page //
-          if (CurrentProject != "Jumbotron") {
-            var nextPage = $("#" + CurrentProject).next('.ProjectsView').prop("id");
-            if (nextPage == null) {
-              nextPage = $("#" + CurrentProject).prev('.ProjectsView').prop("id");
-            }
-            HandlePageArrows(nextPage);
-          }
-        });
       });
 
       // The Indeed icon isn't from FA, so we have to toggle the src on hover to make the effect the same as the ones that are //
